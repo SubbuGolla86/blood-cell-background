ecb210cd97246d4f7ec373209976e745506b58d9
SubbuGolla86 <gollapothusubbu86@gmail.com>
2025-04-17 00:35:00 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "spartan-vine-456818-e9-92f4ab9629cc.json"
 from google.cloud import pubsub_v1
 from concurrent.futures import TimeoutError
 from PIL import Image
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "spartan-vine-456818-e9-92f4ab9629cc.json"
 from bucket import  download_file
 from deep_learning import model_func,predict
 from datastore import *
@@ -23,12 +23,18 @@ subscription_path = 'projects/spartan-vine-456818-e9/subscriptions/blood-cell-ch
 
 def callback(message):
     record_id = message.data.decode('utf-8')
+    print(f'Received message with record ID: {record_id}')
     path = fetch_blood_cell_record(record_id).get("image_path")
-    message.ack()
+    print(f'Fetched image path: {path}')
     download_file(path)
     image = Image.open(path)
     result = predict(image,model_load)
-    update_blood_cell_record(record_id,result)
+    print(f'Prediction result: {result}')
+    update_blood_cell_record(record_id, result)
+    logging.info(f'Updated blood cell record for ID: {record_id} with result')
+    message.ack()
+    logging.info('Message acknowledged')
+    
 
 streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
 print(f'Listening for messages on {subscription_path}')
